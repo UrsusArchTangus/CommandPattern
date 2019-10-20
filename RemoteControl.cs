7d3957b245d9a41d0eaf96383dbbd1503e32c7b0
Winckler T <38361688+UrsusArchTangus@users.noreply.github.com>
2019-10-20 22:43:31 +0200

    Added code

---
+++ RemoteControl.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using CommandPattern.Commands;
+
+namespace CommandPattern
+{
+    class RemoteControl
+    {
+        private Command[] _onCommand;
+        private Command[] _offCommand;
+
+        public RemoteControl(int slotsOnRemote)
+        {
+            // Example use of exception where parameter is an invalid value
+            if (slotsOnRemote < 1)
+            {
+                throw new System.ArgumentException("RemoteControl Parameter cannot be less than 1", "slotsOnRemote");
+            }
+
+            // Create array of command slots/keys on remote
+            _onCommand = new Command[slotsOnRemote];
+            _offCommand = new Command[slotsOnRemote];
+
+            // Fill all slots with NoCommand
+            for (int i = 0; i < slotsOnRemote; i++)
+            {
+                _onCommand[i] = new NoCommand();
+                _offCommand[i] = new NoCommand();
+            }
+
+        }
+
+        public void SetCommand(int slot, Command onCommand, Command offCommand)
+        {
+            // Example use of exception where parameter causes an exception, here slot is out of range
+            try
+            {
+                _onCommand[slot] = onCommand;
+                _offCommand[slot] = offCommand;
+            }
+            catch (System.IndexOutOfRangeException ex)
+            {
+                System.ArgumentException argEx = new System.ArgumentException("RemoteControl.setCommand Parameter is out of range", "slot", ex);
+                throw argEx;
+            }
+
+        }
+
+        public void OnButtonPushed(int slot)
+        {
+            _onCommand[slot].Execute();
+        }
+
+        public void OffButtonPushed(int slot)
+        {
+            _offCommand[slot].Execute();
+        }
+    }
+}
---
+++ RemoteLoader.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandPattern.Commands;
+using CommandPattern.Lights;
+
+namespace CommandPattern
+{
+    class RemoteLoader
+    {
+        static void Main(string[] args)
+        {
+            RemoteControl remoteControl = new RemoteControl(3);
+
+            // Creating objects to control
+            Light garageLight = new Light1("Garage Light");
+            Light livingRoomLight = new Light2("Living Room Light");
+            // Creating commands for objects to control
+            LightOn garageLightOnCommand = new LightOn(garageLight);
+            LightOff garageLightOffCommand = new LightOff(garageLight);
+            LightOn livingRoomLightOnCommand = new LightOn(livingRoomLight);
+            LightOff livingRoomLightOffCommand = new LightOff(livingRoomLight);
+            // Creating Collection for AllLightOff and AllLightOff commands.
+            var LightsOn = new Command[] {garageLightOnCommand, livingRoomLightOnCommand};
+            var LightsOff = new Command[] {garageLightOffCommand, livingRoomLightOffCommand };
+
+        // Creating Macro commands
+            AllLightOn houseLightsOnCommand = new AllLightOn(LightsOn);
+            AllLightOff houseLightsOffCommand = new AllLightOff(LightsOff);
+            
+
+            // Loading commands into the remoteControl
+            remoteControl.SetCommand(0, garageLightOnCommand, garageLightOffCommand);
+            remoteControl.SetCommand(1, livingRoomLightOnCommand, livingRoomLightOffCommand);
+            remoteControl.SetCommand(2, houseLightsOnCommand, houseLightsOffCommand);
+            // Simulating button clicks
+            remoteControl.OnButtonPushed(0);
+            remoteControl.OffButtonPushed(0);
+            remoteControl.OnButtonPushed(1);
+            remoteControl.OffButtonPushed(1);
+            remoteControl.OnButtonPushed(2);
+            remoteControl.OffButtonPushed(2);
+        }
+    }
+}
